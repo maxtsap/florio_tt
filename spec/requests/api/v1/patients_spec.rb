501dbee669a305ccf8647a316ebd7e188346b873
Maksym Tsaplin <maxim.tsaplin@gmail.com>
2025-07-22 17:56:52 +0200

    Cleanup.

--- spec/requests/api/v1/patients_spec.rb
+++ spec/requests/api/v1/patients_spec.rb
@@ -6,9 +6,7 @@ RSpec.describe "Api::V1::Patients", type: :request do
       let(:valid_params) { { patient: { name: "John Doe" } } }
 
       it "creates a new patient and returns api_key" do
-        expect {
-          post "/api/v1/patients", params: valid_params
-        }.to change(Patient, :count).by(1)
+        expect { post "/api/v1/patients", params: valid_params }.to change(Patient, :count).by(1)
         expect(response).to have_http_status(:created)
         json_response = JSON.parse(response.body)
         expect(json_response["api_key"]).not_to be_empty
@@ -19,9 +17,7 @@ RSpec.describe "Api::V1::Patients", type: :request do
       let(:invalid_params) { { patient: { name: "" } } }
 
       it "does not create a new patient" do
-        expect {
-          post "/api/v1/patients", params: invalid_params
-        }.to_not change(Patient, :count)
+        expect { post "/api/v1/patients", params: invalid_params }.to_not change(Patient, :count)
         expect(response).to have_http_status(:unprocessable_entity)
         json_response = JSON.parse(response.body)
         expect(json_response["errors"]).to include("Name can't be blank")
