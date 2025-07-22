2256dd3a518c3ac1887fed29bf0d7beab63dc8bc
Maksym Tsaplin <maxim.tsaplin@gmail.com>
2025-07-22 16:00:38 +0200

    Generate Patient#api_key on create.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,17 @@
 class Patient < ApplicationRecord
+
   validates :name, presence: true
   validates :api_key, presence: true, uniqueness: true
+
+  before_validation :generate_api_key, on: :create
+
+  private
+
+  def generate_api_key
+    self.api_key ||= loop do
+      token = SecureRandom.hex(20)
+      # Ensure the generated token is unique
+      break token unless Patient.exists?(api_key: token)
+    end
+  end
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Patient, type: :model do
   it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:api_key) }
   it { is_expected.to validate_uniqueness_of(:api_key) }
+
+  describe 'callbacks' do
+    it 'generates an API key before creating a patient' do
+      patient = Patient.new(name: 'John Doe')
+      expect(patient.api_key).to be_nil
+      patient.save
+      expect(patient.reload.api_key).not_to be_nil
+    end
+  end
 end
